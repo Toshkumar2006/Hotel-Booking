f0f3398e9cd895916b43ef1d147679d2e49d7923
Tosh Kumar <tk534948@gmail.com>
2026-07-06 15:12:30 +0530

    Remove routes and add header to App component
    Removed routing and added a simple header.

--- hotel-booking/src/App.jsx
+++ hotel-booking/src/App.jsx
@@ -1,16 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-import HotelDetails from "./pages/HotelDetails";
-import NotFound from "./pages/NotFound";
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/hotel/:id" element={<HotelDetails />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <div style={{ padding: 40 }}>
+      <h1>React Working ✅</h1>
+    </div>
   );
 }
 
