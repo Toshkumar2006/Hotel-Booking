cee54f82a109d5f61a68b9a9169b50adf43b1f9e
Tosh Kumar <tk534948@gmail.com>
2026-07-06 15:17:30 +0530

    Implement routing with React Router

--- hotel-booking/src/App.jsx
+++ hotel-booking/src/App.jsx
@@ -1,8 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import HotelDetails from "./pages/HotelDetails";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>React Working ✅</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hotel/:id" element={<HotelDetails />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
