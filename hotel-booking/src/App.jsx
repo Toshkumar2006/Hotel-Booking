a0e58d28da7446cf7c13c141985d51e1995d38bb
Tosh Kumar <tk534948@gmail.com>
2026-07-06 15:20:13 +0530

    Update App.jsx

--- hotel-booking/src/App.jsx
+++ hotel-booking/src/App.jsx
@@ -1,17 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import HotelDetails from "./pages/HotelDetails";
-import NotFound from "./pages/NotFound";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/hotel/:id" element={<HotelDetails />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
