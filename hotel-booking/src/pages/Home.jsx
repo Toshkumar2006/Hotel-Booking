9116c9799481f04dcaa26d562cf118752e30ba45
Tosh Kumar <tk534948@gmail.com>
2026-07-06 13:18:10 +0530

    Create Home.jsx

---
+++ hotel-booking/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Hotel Booking
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
