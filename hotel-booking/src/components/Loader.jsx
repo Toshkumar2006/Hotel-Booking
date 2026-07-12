0d97d0399097b91650b305c3bdfa66cf87d6871a
Tosh Kumar <tk534948@gmail.com>
2026-07-12 06:18:12 +0530

    Delete hotel-booking/src/components directory

--- hotel-booking/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader() {
-  return (
-    <div className="flex justify-center py-20">
-      <div className="h-16 w-16 rounded-full border-4 border-blue-500 border-t-transparent animate-spin"></div>
-    </div>
-  );
-}
-
-export default Loader;
